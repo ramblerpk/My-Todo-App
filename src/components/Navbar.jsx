c93d38037970083019e260a386372a778bdeacc0
ramblerpk <piyushkumarsmith@gmail.com>
2024-12-02 04:52:31 +0530

    Completed the project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between bg-slate-900 text-white py-2'>
+    <nav className='flex justify-around bg-slate-800 text-white py-2'>
         <div className="logo">
             <span className='font-bold text-xl mx-8'>MyTask</span>
         </div>
